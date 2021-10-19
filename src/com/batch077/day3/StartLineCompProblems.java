09e30806f59a85c7302baa30f6a134cee3fff1d2
prasadjamdade <prasadjamdade@hotmail.com>
2021-10-19 14:58:31 +0530

    Welcome Note

---
+++ src/com/batch077/day3/StartLineCompProblems.java
@@ -0,0 +1,7 @@
+package com.batch077.day3;
+
+public class StartLineCompProblems {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison and Computation Program.");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Prasad
+ *
+ */
+module Day3Assignment {
+}
